e09c2fdb7b0278c5c22165ef30830c3889ce90a2
James Mountain <james.mountain@gmail.com>
2016-05-04 16:47:55 +0100

    webpack basic

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+
+    entry: "./src/index",
+
+    resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+
+	devtool: 'source-map',
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+
+    module: {
+
+    	preLoaders: [
+    		{
+    			test: /\.js?/, loader: "eslint-loader", exclude: /node_modules/
+    		}
+	    ],
+        loaders: [
+        	{
+	           	test: /\.jsx?/,
+			   	loaders: ['babel'],
+				include: path.join(__dirname, 'src')
+			}
+        ]
+    }
+ //    ,
+ //    plugins : [
+	// 	new webpack.optimize.UglifyJsPlugin({
+	// 		compress: {
+	// 			warnings: false
+	// 		}
+	// 	})
+	// ]
+
+};
+
+
