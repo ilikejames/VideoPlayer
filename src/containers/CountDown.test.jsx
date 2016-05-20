4671b9da50c4a8e85447e8db1f1739d0b67aee5c
James Mountain <james.mountain@gmail.com>
2016-05-20 20:15:38 +0100

    friday eve fix

--- src/containers/CountDown.test.jsx
+++ src/containers/CountDown.test.jsx
@@ -2,7 +2,25 @@
 // import expect from 'expect'
 // import React from 'react';
 // import { connect, Provider } from 'react-redux'
-// import shallowRender from 'react-shallow-render'
+// import TestUtils from 'react-addons-test-utils'
+// import { CountDown, default as ConnectedCountDown } from './CountDown';
 
-// import { CountDown } from './CountDown';
 
+// describe('containers/<CountDown/>', () => {
+
+// 	it('Should render the <CountDown/> component', () => {
+
+// 		debugger;
+// 		let renderer = TestUtils.createRenderer();
+// 		renderer.render(<CountDown/>)
+// 		let output = renderer.getRenderedOutput()
+
+// 		//let tree = sd.shallowRender(React.createElement(CountDown, {}));
+
+
+// 	});
+
+// });
+
+
+// // https://jamesfriend.com.au/better-assertions-shallow-rendered-react-components
